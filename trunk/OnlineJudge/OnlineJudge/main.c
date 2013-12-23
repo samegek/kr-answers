d2a05a6a3db6003b7532368db080feb65de866da
kangmingxuan <kangmingxuan@d83dfdd0-134b-247f-7471-9b3beed6d4d0>
2013-12-23 12:10:56 +0000

    A better version solution to BNU OJ 4124 based on the others.

--- trunk/OnlineJudge/OnlineJudge/main.c
+++ trunk/OnlineJudge/OnlineJudge/main.c
@@ -1,60 +1,30 @@
 #include <stdio.h>
 
-#define YES 1
-#define NO	0
-
 int facts[11] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
 
-int search(int n, int s, int e);
-int isfactsum(int n, int e);
-
-/* high precision multiplication: the solution to PID 4121. */
+/* The solution to PID 4124. 
+ * This version base on the fact that if n is such a number, it must
+ * consist the number closest to it. 
+ * For example, 126 is of this kind, 126 must contain 120, or any number
+ * smaller than 120 can't compensate the usage of 120. */
 int main()
 {
-	int n;
-	int i, bound;
+	int n, i;
+	while (scanf("%d", &n) && n >= 0){
 
-	scanf("%d", &n);
-	while (n >= 0){
-		for (i = 1; i <= 10; i++)	/* specify the search region */
-		{
-			if (facts[i] > n)
-				break;
+		if (n == 0){
+			printf("NO\n");
+			continue;
 		}
-		if (isfactsum(n, i - 1) == YES)
+		for (i = 9; i >= 0; i--){
+			if (n >= facts[i])
+				n -= facts[i];
+		}
+		
+		if (n == 0)
 			printf("YES\n");
 		else
 			printf("NO\n");
-		scanf("%d", &n);
 	}
-
 	return 0;
-}
-int isfactsum(int n, int e)
-{
-	int i;
-	for (i = 0; i <= e; i++)
-		if (search(n, i, e) == YES)
-			return YES;
-	return NO;
-}
-
-/* isfactsum: check if n is a sum of some factorial */
-int search( int n, int s, int e )
-{
-	int i;
-
-	n -= facts[s];
-	if (n == 0)
-		return YES;
-	else if (n > 0)
-	{
-		for (i = s + 1; i <= e; i++)
-		{
-			if (search(n, i, e) == YES)
-				return YES;
-		}
-	}
-
-	return NO;
 }
