4bb35c6094cce82d3d2e5863f2249301a388fe11
kangmingxuan <kangmingxuan@d83dfdd0-134b-247f-7471-9b3beed6d4d0>
2013-12-23 03:37:07 +0000

    Add solution to BNU OJ 4123.

--- OnlineJudge/OnlineJudge/main.c
+++ OnlineJudge/OnlineJudge/main.c
@@ -1,105 +1,53 @@
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
 
-#define max(a, b) ((a >= b) ? a : b)
-#define MAXN (400+10)
+#define YES 1
+#define NO	0
 
-int op1[MAXN];
-int op2[MAXN];
-int ans[MAXN + MAXN];
+int facts[11] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
+int accessed[11] = {1};
+int cursum;
 
-void sethp(int *, int);
-void hpmul(int *, int *, int *);
-void hpprint(int *, int);
+int isfactsum(int n, int s, int e);
 
 /* high precision multiplication: the solution to PID 4121. */
 int main()
 {
-	double R;
-	int n, scale;
-	while (scanf("%6lf%d", &R, &n)== 2){
-		R *= 100000.0;
-		R = floor(R + 0.5);
-		scale = 5 * n;
-
-		sethp(op1, 1);
-		sethp(op2, R);
-		while(n--){
-			memset(ans, 0, sizeof(ans));		/* set the ans into 0*/
-			hpmul(op1, op2, ans);
-			memcpy(op1, ans, (*ans + 1)*sizeof(int));
-
+	int i, n;
+	scanf("%d", &n);
+	while (n >= 0){
+		for (i = 1; i <= 10; i++)
+		{
+			if (facts[i] >= n)
+				break;
 		}
-		hpprint(op1, scale);
+
+		if (isfactsum(n, 1, i))
+		{
+			printf("YES\n");
+		} 
+		else
+		{
+			printf("NO\n");
+		}
+		scanf("%d", &n);
 	}
 
 	return 0;
 }
-
-/* sethp: convert num into high precision representation */
-void sethp(int *hp, int num)
+/* isfactsum: check if n is a sum of some factorial */
+int isfactsum( int n, int s, int e )
 {
-	int count = 0;
-	do{
-		*(hp + ++count) = num % 10;
-	} while (num /= 10);
+	int i;
 
-	*hp = count;
-}
-
-/* hpmul: multiple to hp(high precision) represented number */
-void hpmul(int *op1, int *op2, int *ans)
-{
-	int i, j, k;
-
-
-	/* multiplication */
-	for (i = 1; i <= *op1; i++){
-		for (j = 1; j <= *op2; j++){
-			k = i + j - 1;
-			*(ans + k) += *(op1 + i) * *(op2 + j);
-			*(ans + k + 1) += *(ans + k) / 10; /* handle the carry */
-			*(ans + k) %= 10;
+	for (i = s; i <= e; i++)
+	{
+		if ((cursum + facts[i]) == n)
+		{
+			return 1;
 		}
-	}
+		else{
 
-	/* count the length of the answer */
-	i = *op1 + *op2;
-	while (i > 0 && *(ans + i) == 0)
-		i--;
-	if (i != 0)
-		*ans = i;
-	else
-		*ans = 1;
-}
-/* hpprint: print high precision values, point specifies the 
- * position that divide the fraction part and the integer part */
-void hpprint(int *hpv, int point)
-{
-	int i, j; 
-	if (point <= *hpv){
-		for (i = *hpv; i > point; i--)
-			putchar(*(hpv + i) + '0');
-		j = 1;
-		while (j <= point && *(hpv + j) == 0)
-			j++;	
-		if (i >= j)
-			putchar('.');
-		for(; i >= j; i--)
-			putchar(*(hpv + i) + '0');
-	}
-	else{
-		putchar('.');
-		j = 1;
-		while (*(hpv + j) == 0)
-			j++;
-		i = point - *hpv;
-		while(i--)
-			putchar('0');
-		for (i = *hpv; i >= j; i--)
-			putchar(*(hpv + i) + '0');
-	}
+		}
 
-	putchar('\n');
+	}
 }
