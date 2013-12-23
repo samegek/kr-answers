596d31bea29cca33a137e2e567092e97371faecb
kangmingxuan <kangmingxuan@d83dfdd0-134b-247f-7471-9b3beed6d4d0>
2013-12-23 11:36:12 +0000

    The first version of solution to BNU OJ 4124.

--- trunk/OnlineJudge/OnlineJudge/main.c
+++ trunk/OnlineJudge/OnlineJudge/main.c
@@ -4,49 +4,54 @@
 #define NO	0
 
 int facts[11] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800};
-int accessed[11];
-int n;
 
-int isfactsum(int n, int s, int e);
+int search(int n, int s, int e);
+int isfactsum(int n, int e);
 
 /* high precision multiplication: the solution to PID 4121. */
 int main()
 {
-	int i;
+	int n;
+	int i, bound;
+
 	scanf("%d", &n);
 	while (n >= 0){
 		for (i = 1; i <= 10; i++)	/* specify the search region */
 		{
-			if (facts[i] >= n)
+			if (facts[i] > n)
 				break;
 		}
-
-		if (isfactsum(n, 1, i))
-		{
+		if (isfactsum(n, i - 1) == YES)
 			printf("YES\n");
-		} 
 		else
-		{
 			printf("NO\n");
-		}
 		scanf("%d", &n);
 	}
 
 	return 0;
 }
+int isfactsum(int n, int e)
+{
+	int i;
+	for (i = 0; i <= e; i++)
+		if (search(n, i, e) == YES)
+			return YES;
+	return NO;
+}
+
 /* isfactsum: check if n is a sum of some factorial */
-int isfactsum( int n, int s, int e )
+int search( int n, int s, int e )
 {
 	int i;
 
-	n -= s;
+	n -= facts[s];
 	if (n == 0)
 		return YES;
 	else if (n > 0)
 	{
 		for (i = s + 1; i <= e; i++)
 		{
-			if (isfactsum(n, i, e) == YES)
+			if (search(n, i, e) == YES)
 				return YES;
 		}
 	}
