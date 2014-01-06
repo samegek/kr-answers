41f79c11dcd8b9974f0e2de5592df30bebf3d588
kangmingxuan <kangmingxuan@d83dfdd0-134b-247f-7471-9b3beed6d4d0>
2014-01-06 09:01:54 +0000

    Add the first solution to BNU OJ 1020.

--- OnlineJudge/OnlineJudge/main.c
+++ OnlineJudge/OnlineJudge/main.c
@@ -1,20 +1,58 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-/* solution to the BNU OJ 1040 */
+
+#define MAXARMY 1000000
+#define MAXDOCT 1000
+#define max(a,b) ((a>=b)?(a):(b))
+
+int army[MAXARMY];
+int doctors[MAXDOCT];
+
+int cmp(const void *, const void *);
+
+/* solution to the BNU OJ 1020 
+ * ATTENTION: the current solution may be over the time limit */
 int main()
 {
-	int t;
-	int n;
-	double r, s;
-	double pi = acos(-1.0);
+	int K; /* num of data groups */
+	int N, M, P;
+	int i, j;
+	int minDoct, maxDoct;
 
-	scanf("%d", &t);
-	while (--t >= 0)
-	{
-		scanf("%lf%d", &r, &n);
-		s = n * 0.5 * r * r * sin(2*pi/n);
-		printf("%.3f\n", s);
+	scanf("%d", &K);
+	while (--K >= 0){
+		scanf("%d%d%d", &N, &M, &P);
+		for (i = 0; i < N; i++){
+			scanf("%d", &army[i]);
+		}
+		qsort(army, N, sizeof(int), cmp);
+		for (i = N-1; i >= 0; i--){
+			/* get number of the accessible doctor 
+			 * be careful with the seatch algorithm! */
+			minDoct = 0;
+			for (j = 1; j < M; j++){
+				if (doctors[j] < doctors[minDoct]){
+					minDoct = j;
+				}
+			}
+			doctors[minDoct] += army[i];
+		}
+
+		maxDoct = 0;
+		for (j = 1; j < M; j++){
+			if (doctors[j] > doctors[maxDoct]){
+				maxDoct = j;
+			}
+		}
+
+	printf("%.3f", doctors[maxDoct] / (double)P);
 	}
 	return 0;
+}
+
+/* cmp: cmpare function used by qsort */
+int cmp(const void *n1, const void *n2)
+{
+	return *(int*)n1 - *(int*)n2;
 }
